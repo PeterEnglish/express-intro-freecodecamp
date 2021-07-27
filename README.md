3918e5f016ca8a6cc4ebd6393f57ba22d508df0c
Peter English <englishpeter29@gmail.com>
2021-07-27 01:35:32 +0100

    Updated myApp.js with code I created on replit.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)
+
+This is part of the introduction to Basic Node and Express on FreeCodeCamp. I did a short, hour-long course where I created routes and middleware, and dealt with querystrings and request parameters to create a simple API with seven different routes, which, among other things, got the time and echoed a word in json format.
--- myApp.js
+++ myApp.js
@@ -1,39 +1,51 @@
 var express = require('express');
 var app = express();
 
+absolutePath = __dirname + "/views/index.html";
+
+app.use(express.urlencoded({extended: false}));
+
+app.use("", function(req, res, next){
+  console.log(req.method+ " " + req.path + " - " + req.ip);
+  next();
+})
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(absolutePath);
+});
+
+app.get('/now', function (req, res, next){
+  const currentTime = new Date().toString();
+  req.time = currentTime;
+  next();
+},
+  function(req, res){
+    res.json({time: req.time});
+  }
+);
+
+app.post('/name', (req, res)=>{
+  res.json({name: req.body.first + " " + req.body.last})
+})
+
+app.get('/:word/echo', (req, res)=>{
+    res.json({echo:req.params.word})
+})
+
+app.get('/name', (req, res)=>{
+  res.json({name: req.query.first + " " + req.query.last})
+})
 
 
+app.get("/json", function(req, res){
+  if(process.env.MESSAGE_STYLE==="uppercase"){
+    res.json({"message": "HELLO JSON"});
+  }else{
+    res.json({"message": "Hello json"})
+  };
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
